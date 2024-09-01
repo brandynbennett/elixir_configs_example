825c456173d062f8c3f8532c262dda25de10ecab
brandynbennett <brandynlbennett@gmail.com>
2024-08-31 23:09:44 -0600

    Move `:url` and `:http` configs inside a `if :prod` conditional
    These ones are tricker to do cleanly because they are more deeply nested. There are values for these in dev and test files that we don't want to clobber that are a mix of compile-time and runtime values
    
    We do separate the `secret_key_base` part because it can cleanly stand-alone as a runtime only config

--- config/runtime.exs
+++ config/runtime.exs
@@ -64,17 +64,20 @@ port = RuntimeConfig.get("PORT", cast: :integer)
 
 config :elixir_configs_example, :dns_cluster_query, RuntimeConfig.get("DNS_CLUSTER_QUERY")
 
-config :elixir_configs_example, ElixirConfigsExampleWeb.Endpoint,
-  url: [host: host, port: 443, scheme: "https"],
-  http: [
-    # Enable IPv6 and bind on all interfaces.
-    # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-    # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-    # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-    ip: {0, 0, 0, 0, 0, 0, 0, 0},
-    port: port
-  ],
-  secret_key_base: secret_key_base
+config :elixir_configs_example, ElixirConfigsExampleWeb.Endpoint, secret_key_base: secret_key_base
+
+if config_env() == :prod do
+  config :elixir_configs_example, ElixirConfigsExampleWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ]
+end
 
 # ## SSL Support
 #
