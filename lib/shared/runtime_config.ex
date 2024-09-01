95b14dc9c118d683406988340d4e539e283e6236
brandynbennett <brandynlbennett@gmail.com>
2024-08-31 22:10:51 -0600

    Add `Shared.RuntimeConfig` module
    This module will be used in `runtime.exs` to make setting default convenient because we can't `import` environment specific files for runtime.

---
+++ lib/shared/runtime_config.ex
@@ -0,0 +1,104 @@
+defmodule Shared.RuntimeConfig do
+  @moduledoc """
+  Use `RuntimeConfig` to make it easier to establish defaults for runtime config values.
+  This for the purpose of keeping compile-time configs and runtime-configs separated.
+  If something can be set at runtime we only want it to be used in `runtime.exs` so that
+  we know where to look for it.
+
+  ## Setup Example
+
+  ```elixir
+  # runtime.exs
+
+  import Config
+
+  defmodule RuntimeConfig do
+    use Shared.RuntimeConfig
+
+    def defaults do
+      %{
+        "DATABASE_URL" => [test: "localhost", dev: "localhost"]
+      }
+    end
+  end
+  ```
+
+  ## Retrieving environment variables
+
+  Use the `RuntimeConfig.get/2` function to fetch your environment variables. If a
+  default has not been defined for the environment in `c:defaults/0` an
+  error will be raised.
+
+  If you want the value returned in a form other than a String, you can pass an optional
+  `:cast` option to return a different type. Supported options include:
+
+  * `:integer`
+  * `:boolean`
+
+  ```
+    iex>RuntimeConfig.get("DATABASE_URL")
+    "localhost"
+
+    iex>RuntimeConfig.get("PORT", cast: :integer)
+    4000
+
+    iex>RuntimeConfig.get("FOO")
+    (RuntimeError) environment variable FOO not set and no default for :dev
+  ```
+  """
+
+  import Config
+
+  @type envs :: {:prod, String.t()} | {:dev, String.t()} | {:test, String.t()}
+
+  @callback defaults() :: %{
+              String.t() => [envs()]
+            }
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Shared.RuntimeConfig
+
+      def get(envvar, opts \\ []) do
+        Shared.RuntimeConfig.get(envvar, defaults(), opts)
+      end
+    end
+  end
+
+  def get(envvar, defaults, opts \\ []) do
+    value = System.get_env(envvar) || default(envvar, defaults, config_env())
+
+    case Keyword.get(opts, :cast) do
+      :integer ->
+        value && String.to_integer(value)
+
+      :boolean ->
+        value in ["1", "true", "TRUE", true]
+
+      # ... more casts
+
+      nil ->
+        value
+    end
+  end
+
+  defp default(envvar, defaults, env) when is_map(defaults) do
+    case Map.get(defaults, envvar) do
+      nil ->
+        raise("environment variable #{envvar} not set and no defaults configured")
+
+      env_defaults ->
+        env_default(envvar, env_defaults, env)
+    end
+  end
+
+  defp env_default(envvar, defaults, env) do
+    case Keyword.get(defaults, env) do
+      nil ->
+        raise("environment variable #{envvar} not set and no default for #{inspect(env)}")
+
+      value ->
+        value
+    end
+  end
+end
