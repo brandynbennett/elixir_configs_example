420779241a8571357ef44f4d423b6996ddacd2d1
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:42:06 -0600

    Delete importing of env specific configs
    We can do this because these configs are all empty now

--- config/config.exs
+++ config/config.exs
@@ -13,7 +13,3 @@ import_config "compile/endpoint.exs"
 import_config "compile/logger.exs"
 import_config "compile/mailer.exs"
 import_config "compile/phoenix.exs"
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{config_env()}.exs"
