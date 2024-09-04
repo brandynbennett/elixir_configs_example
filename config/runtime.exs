72ab7c158b5b11ac90066f3cf3a2861b01d2f162
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:11:03 -0600

    Delete comment about prod only block
    We don't want to leave future readers confused about this now outdated comment

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,7 +5,6 @@ import Config
 # system starts, so it is typically used to load production configuration
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
 
 defmodule RuntimeConfig do
   use Shared.RuntimeConfig
