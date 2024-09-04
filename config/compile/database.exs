c26dd7e3dfcb7091f054a7be6e3f16af2c6f6aa7
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:36:49 -0600

    Pull Ecto configs out of `test.exs`

--- config/compile/database.exs
+++ config/compile/database.exs
@@ -13,3 +13,17 @@ if config_env() == :dev do
     stacktrace: true,
     show_sensitive_data_on_connection_error: true
 end
+
+if config_env() == :test do
+  # Configure your database
+  #
+  # The MIX_TEST_PARTITION environment variable can be used
+  # to provide built-in test partitioning in CI environment.
+  # Run `mix help test` for more information.
+  config :elixir_configs_example, ElixirConfigsExample.Repo,
+    username: "postgres",
+    password: "postgres",
+    hostname: "localhost",
+    database: "elixir_configs_example_test#{System.get_env("MIX_TEST_PARTITION")}",
+    pool: Ecto.Adapters.SQL.Sandbox
+end
