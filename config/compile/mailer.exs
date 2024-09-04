9e04d5280a82b985f75bf9785d9ba113a197e7a2
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:38:39 -0600

    Move mailer configs out of test.exs

--- config/compile/mailer.exs
+++ config/compile/mailer.exs
@@ -13,3 +13,11 @@ if config_env() == :dev do
   # Disable swoosh api client as it is only required for production adapters.
   config :swoosh, :api_client, false
 end
+
+if config_env() == :test do
+  # In test we don't send emails
+  config :elixir_configs_example, ElixirConfigsExample.Mailer, adapter: Swoosh.Adapters.Test
+
+  # Disable swoosh api client as it is only required for production adapters
+  config :swoosh, :api_client, false
+end
--- config/test.exs
+++ config/test.exs
@@ -1,11 +1,5 @@
 import Config
 
-# In test we don't send emails
-config :elixir_configs_example, ElixirConfigsExample.Mailer, adapter: Swoosh.Adapters.Test
-
-# Disable swoosh api client as it is only required for production adapters
-config :swoosh, :api_client, false
-
 # Print only warnings and errors during test
 config :logger, level: :warning
 
