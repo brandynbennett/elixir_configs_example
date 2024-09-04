1b6be6bfaafd98b2e76a4f6bd4851bbfe403fd66
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:35:31 -0600

    Pull logger configs out of dev

--- config/compile/logger.exs
+++ config/compile/logger.exs
@@ -4,3 +4,8 @@ import Config
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
+
+if config_env() == :dev do
+  # Do not include metadata nor timestamps in development logs
+  config :logger, :console, format: "[$level] $message\n"
+end
--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,4 @@
 import Config
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
-
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
