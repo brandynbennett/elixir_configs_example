50dcee2f507dae158109f200cc8fcb701ed30cb8
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:36:03 -0600

    Pull mailer configs out of `dev.exs`

--- config/compile/mailer.exs
+++ config/compile/mailer.exs
@@ -8,3 +8,8 @@ import Config
 # For production it's recommended to configure a different adapter
 # at the `config/runtime.exs`.
 config :elixir_configs_example, ElixirConfigsExample.Mailer, adapter: Swoosh.Adapters.Local
+
+if config_env() == :dev do
+  # Disable swoosh api client as it is only required for production adapters.
+  config :swoosh, :api_client, false
+end
--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,1 @@
 import Config
-
-# Disable swoosh api client as it is only required for production adapters.
-config :swoosh, :api_client, false
