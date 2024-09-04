534c9d7ff4bef009673a8cc06a38619432a1d7d9
brandynbennett <brandynlbennett@gmail.com>
2024-09-03 22:25:22 -0600

    Start mailer.exs config
    Uses mailer config from `config.exs` as a starter

---
+++ config/compile/mailer.exs
@@ -0,0 +1,10 @@
+import Config
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :elixir_configs_example, ElixirConfigsExample.Mailer, adapter: Swoosh.Adapters.Local
